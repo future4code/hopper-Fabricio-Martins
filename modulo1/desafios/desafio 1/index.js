b27cf48c41594baaaed0fabb435a24e2e0b606d5
FabricioMSilva <fabriciomdasilva8@gmail.com>
2022-02-12 09:00:30 -0300

    save game

---
+++ modulo1/desafios/desafio 1/index.js
@@ -0,0 +1,18 @@
+
+// Aqui variavel recebe X valor
+let primeiroNum = Number(prompt("Digite o Primeiro numero:  "))
+
+// Aqui variavel recebe Y valor
+let segundoNum = Number(prompt("Digite o Segundo numero:  "))
+
+// Aqui acontece a soma de X e Y
+let soma = primeiroNum + segundoNum
+
+// Aqui acontece a multiplicado de X e Y
+let multiplicado = primeiroNum * segundoNum
+
+// Agora informa resultados ao usuario
+
+console.log("1. O primeiro número somado ao segundo número resulta em:" ,soma)
+console.log("2. O primeiro número multiplicado pelo segundo número resulta em:",multiplicado)
+
