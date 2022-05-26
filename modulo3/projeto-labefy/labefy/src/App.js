0a0900d0550d5a6011cf366d865fd655d2a29faa
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2022-05-26 20:24:40 -0300

    inciando

---
+++ modulo3/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { CriarPlayList } from './components/criarPlayList';
+function App() {
+  return (
+    <div className="App">
+      
+          <CriarPlayList/>
+     
+    </div>
+  );
+}
+
+export default App;
